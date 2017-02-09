8a115ab342aed72146c16711bad7ebabbb01e83f
dmw <spamdog@gmail.com>
2017-02-09 22:50:55 +1000

    - Added row pick control

---
+++ src/ColorPickerWPF/ColorPickRow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using ColorPickerWPF.Code;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace ColorPickerWPF
+{
+    /// <summary>
+    /// Interaction logic for ColorPickRow.xaml
+    /// </summary>
+    public partial class ColorPickRow : UserControl
+    {
+        public Color Color;
+
+        public ColorPickRow()
+        {
+            InitializeComponent();
+        }
+
+        private void PickColorButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Color color;
+            if (ColorPickerWindow.ShowDialog(out color))
+            {
+                SetColor(color);
+            }
+        }
+
+        public void SetColor(Color color)
+        {
+            Color = color;
+            HexLabel.Text = color.ToHexString();
+            ColorDisplayGrid.Background = new SolidColorBrush(color);
+        }
+    }
+}
